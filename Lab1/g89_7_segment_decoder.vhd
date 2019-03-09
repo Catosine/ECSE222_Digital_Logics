2e5a84d8973eb9ddb1675e2f87938e004e4bee8a
Cytosine <35353368+Catosine@users.noreply.github.com>
2019-03-08 19:05:12 -0500

    Update g89_7_segment_decoder.vhd

--- Lab1/g89_7_segment_decoder.vhd
+++ Lab1/g89_7_segment_decoder.vhd
@@ -5,8 +5,8 @@ use IEEE.NUMERIC_STD.ALL;
 -- Declaring the entity of the decoder
 entity g89_7_segment_decoder is
 	Port (
-		code 		: in 	std_logic_vector(3 downto 0);
-      		segments 	: out 	std_logic_vector(6 downto 0)
+		code 		: in 	std_logic_vector(3 downto 0);	-- 4-bit binary input
+      		segments 	: out 	std_logic_vector(6 downto 0)	-- 7-bit binary output
 	);
 end g89_7_segment_decoder;
 
@@ -17,23 +17,23 @@ begin
 	process(code)
   		begin 
     		case code is 
-      			when "0000" => segments <= "1000000"; -- 0
-      			when "0001" => segments <= "1111001"; -- 1
-      			when "0010" => segments <= "0100100"; -- 2
-      			when "0011" => segments <= "0110000"; -- 3
-      			when "0100" => segments <= "0011001"; -- 4
-      			when "0101" => segments <= "0010010"; -- 5
-      			when "0110" => segments <= "0000010"; -- 6
-      			when "0111" => segments <= "1111000"; -- 7
-      			when "1000" => segments <= "0000000"; -- 8
-      			when "1001" => segments <= "0010000"; -- 9
-			when "1010" => segments <= "0001000"; -- A
-			when "1011" => segments <= "0000011"; -- B
-			when "1100" => segments <= "1000110"; -- C
-			when "1101" => segments <= "0100001"; -- D
-			when "1110" => segments <= "0000110"; -- E
-			when "1111" => segments <= "0001110"; -- F
-      			when others => segments <= "1111111"; -- Otherwise
+      			when "0000" => segments <= "1000000"; 	-- 0 for 7-segment display
+      			when "0001" => segments <= "1111001";	-- 1 for 7-segment display
+      			when "0010" => segments <= "0100100"; 	-- 2 for 7-segment display
+      			when "0011" => segments <= "0110000"; 	-- 3 for 7-segment display
+      			when "0100" => segments <= "0011001"; 	-- 4 for 7-segment display
+      			when "0101" => segments <= "0010010"; 	-- 5 for 7-segment display
+      			when "0110" => segments <= "0000010"; 	-- 6 for 7-segment display
+      			when "0111" => segments <= "1111000"; 	-- 7 for 7-segment display
+      			when "1000" => segments <= "0000000"; 	-- 8 for 7-segment display
+      			when "1001" => segments <= "0010000"; 	-- 9 for 7-segment display
+			when "1010" => segments <= "0001000"; 	-- A for 7-segment display
+			when "1011" => segments <= "0000011"; 	-- B for 7-segment display
+			when "1100" => segments <= "1000110"; 	-- C for 7-segment display
+			when "1101" => segments <= "0100001"; 	-- D for 7-segment display
+			when "1110" => segments <= "0000110"; 	-- E for 7-segment display
+			when "1111" => segments <= "0001110"; 	-- F for 7-segment display
+      			when others => segments <= "1111111"; 	-- Otherwise
     		end case;
   	end process;
 end behaviour;
