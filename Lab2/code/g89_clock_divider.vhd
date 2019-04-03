3f2d0c24deb13209ababc617630c684aae5170b6
Cytosine <35353368+Catosine@users.noreply.github.com>
2019-04-03 13:42:45 -0400

    Edited format

--- Lab2/code/g89_clock_divider.vhd
+++ Lab2/code/g89_clock_divider.vhd
@@ -3,15 +3,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 entity g89_clock_divider is
    Port (
-       enable	:	in  std_logic;		-- divider will only work when enable is 1															-- divider will only work when enable is 1
-       reset   	:  	in  std_logic;		-- divider will be reset when enable is 0														-- divider will be reset when reset is 0
-       clk     	:  	in  std_logic;		-- divider will start a change everytime when clk is 1														-- divider will start everytime at the rising edge of clk
-       en_out  	:  	out std_logic		-- divided enable output													-- result
+       enable	:	in  std_logic;		-- divider will only work when enable is 1
+       reset   	:  	in  std_logic;		-- divider will be reset when enable is 0
+       clk     	:  	in  std_logic;		-- divider will start a change everytime when clk is 1
+       en_out  	:  	out std_logic		-- divided enable output
    );
 end g89_clock_divider;
 
 architecture down_counter of g89_clock_divider is
-	signal temp	:	std_logic;																-- current state
+	signal temp	:	std_logic;							-- current state
 	signal counter	:	std_logic_vector (18 downto 0) := "1111010000100011111";	-- output 1 every after 500,000 times of clock
 begin
 	process(enable, reset, clk)
